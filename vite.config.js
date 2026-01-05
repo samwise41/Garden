e5906859ee34a6ed085c2c92d65e255664e31428
samwise41 <134550253+samwise41@users.noreply.github.com>
2026-01-05 14:07:03 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // CHANGE THIS LINE BELOW:
-  base: 'https://github.com/samwise41/Garden', 
+  base: '/Garden/',  // <--- Just the repo name with slashes
 })
