ff8dfe7657799ca0f6f84f624c440a114e2eea73
samwise41 <134550253+samwise41@users.noreply.github.com>
2026-01-05 12:45:17 -0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // CHANGE THIS LINE BELOW:
+  base: '/YOUR_REPO_NAME/', 
+})
